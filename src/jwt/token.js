13756029d0d7d1d2991502c92e1002b8f0434247
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-01-23 01:30:10 +0600

    JWT middleware implemented

--- src/jwt/token.js
+++ src/jwt/token.js
@@ -3,5 +3,8 @@ import  Jwt  from "jsonwebtoken"
 const CreateToken = async () =>{
   const token = await Jwt.sign({_id:"65ad7e063a89dc97a1801722"}, "615479e7b63e61e1552f4b689009ee1c7e2f589b5032d7bb07b9dcdc9e27e998eb90ea5ae498e0e1502e9c3ce96ee693d580d8f6285b5ed8ec7d3bd2b83b76ef" ,{expiresIn : '1h'})
   console.log(token)
+
+  const verify = await Jwt.verify(token, "615479e7b63e61e1552f4b689009ee1c7e2f589b5032d7bb07b9dcdc9e27e998eb90ea5ae498e0e1502e9c3ce96ee693d580d8f6285b5ed8ec7d3bd2b83b76ef")
+  console.log('jwt verify checking' , verify)
 }
  export default CreateToken
