bc187512a56b4d5b737915bc5dd979f8b0d8991f
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-01-23 19:08:28 +0600

    jwt token middleware updated

--- src/jwt/middleware/auth.js
+++ src/jwt/middleware/auth.js
@@ -2,10 +2,17 @@ import  Jwt   from "jsonwebtoken";
 const auth = async (req , res, next) =>{
 
     try {
-        const token = req.cookies?.token
+        const token = req?.cookies?.token
        if(!token){
-        res.status(401).send({massage: 'Not Authorized'})
+        res.status(401).send({massage: 'Unauthorized'})
        }
+       Jwt.verify(token, process.env.SECRET_TOKEN, (error , decoded)=>{
+        if(error){
+            res.status(401).send({massage: 'Unauthorized'})  
+        }
+        req.user = decoded
+        next()
+       })
     } catch (error) {
         res.status(401).send(error)
     }
