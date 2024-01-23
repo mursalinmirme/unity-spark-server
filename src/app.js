8fe43a937a4c01137897475050c2d16b02db45d3
mdabdurrahman07 <jamilrahman2001@gmail.com>
2024-01-23 17:02:58 +0600

    JWT related issue fixed

--- src/app.js
+++ src/app.js
@@ -7,7 +7,10 @@ import allDeleteRoutes from "./routes/allDeleteRoutes.js";
 import cookieParser from "cookie-parser";
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:5173', ' http://localhost:5174'],
+    credentials: true
+}));
 app.use(express.json());
 app.use(cookieParser())
 
