7ec5265eda7ebbf4864f31ecad6ba5ea5aad550e
nisakib07 <websakib07@gmail.com>
2024-01-25 19:51:24 +0600

    [model : jobAds model updated]

--- src/models/jobAds.js
+++ src/models/jobAds.js
@@ -1,58 +1,50 @@
 import mongoose from "mongoose";
 
 const jobAdsSchema = mongoose.Schema({
-    title: {
-        type: String,
-        require: true,
-        minlength: [3 , "minimum length of the feedback title should be 3"],
-        maxlength: [100, "maximum length of the feedback title should be 10"],
-        trim: true,
-    },
-    jobType: {
-        type: String,
-        require: true,
-        minlength: [3 , "minimum length of the feedback title should be 3"],
-        maxlength: [100, "maximum length of the feedback title should be 10"],
-    },
-    employeType: {
-        type: String,
-        require: true,
-        minlength: [3 , "minimum length of the feedback title should be 3"],
-        maxlength: [100, "maximum length of the feedback title should be 10"],
-    },
-    salary: {
-        type: String,
-        require: true,
-        min: [3, "minimum salary of the employee  should be 3k"],
-        max: [200, "maximum salary of the employee  should be 200k"]
-    },
-    description: {
-        type: String,
-        require: true,
-    },
-    jobPostedAt: {
-        type: Date,
-        default: Date.now(),
-    }
-
-})
-
+  job_title: {
+    type: String,
+    require: true,
+    trim: true,
+  },
+  job_category1: {
+    type: String,
+    require: true,
+  },
+  job_category2: {
+    type: String,
+    require: true,
+  },
+  position: {
+    type: String,
+    require: true,
+  },
+  salary: {
+    type: String,
+    require: true,
+  },
+  job_description: {
+    type: String,
+    require: true,
+  },
+  required_Skills: {
+    type: Array,
+    required: true,
+  },
+  additional_Require: {
+    type: Array,
+  },
+  education_Require: {
+    type: Array,
+  },
+  benefits: {
+    type: Array,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now(),
+  },
+});
 
 const jobAds = mongoose.model("job_ads", jobAdsSchema);
 
-
 export default jobAds;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
