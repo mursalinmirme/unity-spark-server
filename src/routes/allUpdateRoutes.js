26ad553615cbe9a7383f51979264a668909ec203
nisakib07 <websakib07@gmail.com>
2024-01-30 21:05:09 +0600

    [models(users) : upsert added]

--- src/routes/allUpdateRoutes.js
+++ src/routes/allUpdateRoutes.js
@@ -2,35 +2,21 @@ import { app } from "../app.js";
 import users from "../models/users.js";
 
 const allUpdateRoutes = () => {
-
-    // update user profile info
-    app.put("/users/:email", async(req, res) => {
-        try {
-            const updateEmail = req.params.email;
-            const updateInfo = req.body;
-            const result = await users.updateOne({email: updateEmail}, {$set: updateInfo});
-            res.send(result)
-        } catch (error) {
-            res.status(400).send("Something went wrong.")
-        }
-    })
-
-
-
-
-
-
-    
-}
+  // update user profile info
+  app.put("/users/:email", async (req, res) => {
+    try {
+      const updateEmail = req.params.email;
+      const updateInfo = req.body;
+      const result = await users.updateOne(
+        { email: updateEmail },
+        { $set: updateInfo },
+        { upsert: true }
+      );
+      res.send(result);
+    } catch (error) {
+      res.status(400).send("Something went wrong.");
+    }
+  });
+};
 
 export default allUpdateRoutes;
-
-
-
-
-
-
-
-
-
-
