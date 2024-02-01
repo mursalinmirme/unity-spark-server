da9c20d8dc10f29f8b8c67bf5b8fb9fcb01f7d8b
mizan-chowdhury <mizan1034@gmail.com>
2024-02-01 10:59:54 +0600

    create put api for job ads edit part

--- src/routes/allUpdateRoutes.js
+++ src/routes/allUpdateRoutes.js
@@ -1,4 +1,5 @@
 import { app } from "../app.js";
+import jobAds from "../models/jobAds.js";
 import jobapplications from "../models/jobapplications.js";
 import users from "../models/users.js";
 
@@ -18,20 +19,38 @@ const allUpdateRoutes = () => {
       res.status(400).send("Something went wrong.");
     }
   });
-  // job applications update 
-  app.put('/job_applications/:id' , async (req , res) => {
+  // job applications update
+  app.put("/job_applications/:id", async (req, res) => {
     try {
-      const data = req?.body
-      const id = req.params.id
+      const data = req?.body;
+      const id = req.params.id;
       // console.log(data)
-    const result = await jobapplications.updateOne({_id : id},
-         { $set: data },
-        { upsert: true });
-        res.send(result)
+      const result = await jobapplications.updateOne(
+        { _id: id },
+        { $set: data },
+        { upsert: true }
+      );
+      res.send(result);
     } catch (error) {
       res.status(400).send(error.message);
     }
-  })
+  });
+
+  // job-ads update
+  app.put("/job-ads/:id", async (req, res) => {
+    try {
+      const id = req.params.id;
+      const updatedJob = req.body;
+      const result = await jobAds.updateOne(
+        { _id: id },
+        { $set: updatedJob },
+        { upsert: true }
+      );
+      res.send(result);
+    } catch (error) {
+      console.log(error);
+    }
+  });
 };
 
 export default allUpdateRoutes;
