0b3ab12e6dd241bb381f32aed716a0f43a3972df
mizan-chowdhury <mizan1034@gmail.com>
2024-02-01 08:24:34 +0600

    create api and model for apply job

--- src/routes/allPostRoutes.js
+++ src/routes/allPostRoutes.js
@@ -1,60 +1,63 @@
 import { app } from "../app.js";
+import applyJob from "../models/applyjobs.js";
 import feedback from "../models/feedback.js";
 import jobAds from "../models/jobAds.js";
 import users from "../models/users.js";
 
 // All Post Requests
 const allPostRoutes = () => {
-    // user sign up route
-    app.post('/users', async(req, res) => {
-        try {
-            const newUser = req.body;
-        const existingUser = await users.findOne({email: newUser.email})
-        if(existingUser){
-            return res.send("users already exist!")
-        }
-        const usersModel = new users(newUser)
-        const result = await usersModel.save();
-        res.send(result); 
-        } catch (error) {
-            res.send("Something went wrong.")
-        }                
-    })
-    
+  // user sign up route
+  app.post("/users", async (req, res) => {
+    try {
+      const newUser = req.body;
+      const existingUser = await users.findOne({ email: newUser.email });
+      if (existingUser) {
+        return res.send("users already exist!");
+      }
+      const usersModel = new users(newUser);
+      const result = await usersModel.save();
+      res.send(result);
+    } catch (error) {
+      res.send("Something went wrong.");
+    }
+  });
 
-    // job ads post
-    app.post('/job-ads', async (req, res) => {
-        try {
-            const newJobAds = req.body;
-            const jobAdsModel = new jobAds(newJobAds);
-            const result = await jobAdsModel.save();
-            res.send(result);
-        } catch (error) {
-            console.log("Something went wrong.");
-        }
-    })
+  // job ads post
+  app.post("/job-ads", async (req, res) => {
+    try {
+      const newJobAds = req.body;
+      const jobAdsModel = new jobAds(newJobAds);
+      const result = await jobAdsModel.save();
+      res.send(result);
+    } catch (error) {
+      console.log("Something went wrong.");
+    }
+  });
 
-    // post a feedback from employee
-    app.post('/feedbacks', async (req, res) => {
-        // post a feedback
-        try {
-            const newFeedback = req.body;
-            const feedbackModel = new feedback(newFeedback);
-            const result = await feedbackModel.save();
-            res.send(result);
-        } catch (error) {
-            console.log("Something went wrong.");
-        }
-    })
+  // post a feedback from employee
+  app.post("/feedbacks", async (req, res) => {
+    // post a feedback
+    try {
+      const newFeedback = req.body;
+      console.log(newFeedback);
+      const feedbackModel = new feedback(newFeedback);
+      const result = await feedbackModel.save();
+      res.send(result);
+    } catch (error) {
+      console.log("Something went wrong.");
+    }
+  });
 
+  app.post("/job_applications", async (req, res) => {
+    try {
+      const newApplyJob = req.body;
+      const applyJobModel = new applyJob(newApplyJob);
+      const result = await applyJobModel.save();
+      res.send(result);
+    } catch (error) {
+        console.log("Something went wrong.");
+    }
+  });
+};
 
-
-
-
-    
-
-}
-
-
-export default allPostRoutes
-
+export default allPostRoutes;
