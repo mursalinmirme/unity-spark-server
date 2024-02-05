0abdc3bcd1e32da905721317133a171cdbb30a31
nisakib07 <websakib07@gmail.com>
2024-02-05 21:42:55 +0600

    GET API for leaves of unique user created

--- src/routes/allGetRoutes.js
+++ src/routes/allGetRoutes.js
@@ -337,41 +337,47 @@ const allGetRoutes = () => {
     }
   });
 
-
   // get all leave request
-  app.get("/leaves", async(req, res) => {
-     try {
-      const result = await leaves.find({status: "Pending"}).populate("user");
+  app.get("/leaves", async (req, res) => {
+    try {
+      const result = await leaves.find({ status: "Pending" }).populate("user");
       res.send(result);
-     } catch (error) {
-       console.log(error.message);
-     }
-  })
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
   // get all confiremed leave request
-  app.get("/leaves-confirmed", async(req, res) => {
-     try {
-      const result = await leaves.find({status: "Confirmed"}).populate("user");
+  app.get("/leaves-confirmed", async (req, res) => {
+    try {
+      const result = await leaves
+        .find({ status: "Confirmed" })
+        .populate("user");
       res.send(result);
-     } catch (error) {
-       console.log(error.message);
-     }
-  })
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
   // get all rejected leave request
-  app.get("/leaves-rejected", async(req, res) => {
-     try {
-      const result = await leaves.find({status: "Rejected"}).populate("user");
+  app.get("/leaves-rejected", async (req, res) => {
+    try {
+      const result = await leaves.find({ status: "Rejected" }).populate("user");
       res.send(result);
-     } catch (error) {
-       console.log(error.message);
-     }
-  })
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 
-  // get a specific leave request
+  // get leave requests of individual user
 
-
-
-
-
+  app.get("/leaves/:email", async (req, res) => {
+    try {
+      const userEmail = req.params.email;
+      const result = await leaves.find({ email: userEmail });
+      res.send(result);
+    } catch (error) {
+      console.log(error.message);
+    }
+  });
 }; //ending all get routes brackets
 
 export default allGetRoutes;
