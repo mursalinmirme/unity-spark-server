4312c0404ddb5fdf1add5b5d011623b2be05dc3b
mizan-chowdhury <mizan1034@gmail.com>
2024-03-07 04:12:32 +0600

    created api for course status updated

--- src/models/mycourse.js
+++ src/models/mycourse.js
@@ -1,30 +1,33 @@
 import mongoose from "mongoose";
 
-
 const myCourseSchema = mongoose.Schema({
-    uniqueID: {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "courses",
-        require: true
-    },
-    userEmail: {
-        type: String,
-        require: true 
-    },
-    CourseTitle:{
-        type: String,
-        require: true  
-    },
-    CourseBanner: {
-        type: String,
-        require: true
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-      },
-})
+  uniqueID: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "courses",
+    require: true,
+  },
+  userEmail: {
+    type: String,
+    require: true,
+  },
+  CourseTitle: {
+    type: String,
+    require: true,
+  },
+  CourseBanner: {
+    type: String,
+    require: true,
+  },
+  CourseStatus: {
+    type: String,
+    require: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
 
-const myCourse = mongoose.model("my_courses" , myCourseSchema)
+const myCourse = mongoose.model("my_courses", myCourseSchema);
 
-export default myCourse
+export default myCourse;
